a15188b792e0db1e7ae7add4de9251cb4f95afc8
Pavel Maslov <pavel.masloff@gmail.com>
2022-04-20 08:19:26 +0200

    comments

--- rules/requiredtags.py
+++ rules/requiredtags.py
@@ -8,25 +8,25 @@ class RequiredTags(CloudFormationLintRule):
     shortdesc = 'Check if Tags are present and have required keys'
     description = 'Check Tags for resources'
     tags = ['resources', 'tags']
+    required_tags = ['env']
 
     def match(self, cfn):
         """Check if Tags are present and have required keys"""
 
         matches = []
-        required_tags = ['env']
 
+        # Check if Tags property is set
         all_resources = cfn.search_deep_keys('Properties')
         for resource in all_resources:
             if 'Tags' not in resource[-1]:
-                message = "One or more tags ({0}) are missing on the resource: {1}".format(', '.join(required_tags), resource[:-1][1])
+                message = "One or more tags ({0}) are missing on the resource: {1}".format(', '.join(self.required_tags), resource[:-1][1])
                 matches.append(RuleMatch(resource, message))
 
+        # Check if Required tags are defined
         all_tags = [x for x in cfn.search_deep_keys('Tags') if x[0] == 'Resources']
-
         for all_tag in all_tags:
             all_keys = [d.get('Key') for d in all_tag[-1]]
-
-            for required_tag in required_tags:
+            for required_tag in self.required_tags:
                 if required_tag not in all_keys:
                     message = "Tag '{0}' missing for resource '{1}'"
                     matches.append(RuleMatch(all_tag[:-1], message.format(required_tag, all_tag[:-1][1])))
